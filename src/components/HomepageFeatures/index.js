8ab25ebde1f88d27daa819d8a81a165f2ee9c479
Dnevend <Dnevend@gmail.com>
2025-11-05 21:30:56 +0800

    style: update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -7,19 +7,19 @@ const FeatureList = [
   {
     title: '知识库',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: <>系统性再整理</>,
+    // description: <>系统性再整理</>,
     linkTo: '/docs/intro'
   },
   {
     title: '记录',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: <>有营养的信息和内容</>,
+    // description: <>有营养的信息和内容</>,
     linkTo: '/mark/2024'
   },
   {
     title: '博客',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: <>最美的梨花即将被写出</>,
+    // description: <>最美的梨花即将被写出</>,
     linkTo: '/blog'
   },
 ];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,26 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
+import React, { useState } from "react";
+import clsx from "clsx";
 // import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import styles from './index.module.css';
-import ZhanXiFuChangLiu from '../../static/img/zxfcl.png';
-import YunWoYiShangLeng from '../../static/img/ywysl.png';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import styles from "./index.module.css";
+import ZhanXiFuChangLiu from "../../static/img/zxfcl.png";
+import YunWoYiShangLeng from "../../static/img/ywysl.png";
 
 const taglines = [
-  '江湖的孩子，江湖边老',
-  '在战争中学习战争',
-  '不要恐惧，恐惧吞蚀灵魂',
-  '聪明意味着把事情想清楚，愚蠢意味着直接给出结论而逃避思考。',
-  '你不能搞混两样东西，一是你终将胜利的信念，这是你最输不起的；二是训练自己直面残酷现实，无论现实多残酷。',
-  '一个人应该有能力换尿布、策划侵略、杀猪、给轮船掌舵、设计建筑物、写十四行诗、平衡收支、造墙、接骨、安慰临终之人、写菜单、发布命令、合作、独立工作、解方程、分析新问题、施肥、写电脑程序、做可口的饭菜、有效地战斗、英勇地死去。',
-]
+  "你不能搞混两样东西，一是你终将胜利的信念，这是你最输不起的；二是训练自己直面残酷现实，无论现实多残酷。",
+];
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  const [showTagline, setShowTagline] = useState(0);
+  const [showTagline] = useState(0);
 
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
-        <p className={styles.heroSubtitle}>
-          “{taglines[showTagline]}”
-        </p>
-        <div className={styles.buttons}>
-          <button onClick={() => {
-            setShowTagline(index => index >= taglines.length - 1 ? 0 : index + 1);
-          }}>Next</button>
-        </div>
+        <p className={styles.heroSubtitle}>“{taglines[showTagline]}”</p>
       </div>
 
       <img src={ZhanXiFuChangLiu} alt="展席俯長流" className={styles.zxfcl} />
@@ -44,9 +32,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
 
   return (
-    <Layout
-      title={`Welcome`}
-      description="在战争中学习战争">
+    <Layout title={`Welcome`} description="在战争中学习战争">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
